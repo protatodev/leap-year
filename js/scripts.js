bb1b80d91103cc259960320eddb08e204a66d094
protatodev <thaddonaghue@yahoo.com>
2018-06-05 21:40:23 -0700

    finalize front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,27 @@ $(document).ready(function() {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
-  });
 
-  function leapYear(year) {
+    $(".year").text(year);
 
-    if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-      return true;
+    if(!result) {
+      $(".not").text("not");
     } else {
-
-      return false;
+      $(".not").text("");
     }
+
+    $("#result").show();
+
+  });
+});
+
+function leapYear(year) {
+
+  if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+
+    return false;
   }
 
-});
+};
