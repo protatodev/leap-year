7ac2efa38c3f84a7ac1c6b70c4d5f0d61beff036
protatodev <thaddonaghue@yahoo.com>
2018-06-05 21:18:49 -0700

    add functionality for spec detecting 100 divisible leap year

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,10 @@ $(document).ready(function() {
 
   function leapYear(year) {
 
-    if((year % 4) === 0) {
+    if((year % 100) === 0) {
+
+      return false;
+    } else if((year % 4) === 0) {
 
       return true;
     } else {
