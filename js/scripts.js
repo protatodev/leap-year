be29f07ec62b04425c8d0b377f77b313c1a1f6ec
protatodev <thaddonaghue@yahoo.com>
2018-06-05 21:22:26 -0700

    refactor code for readability and efficiency

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,7 @@ $(document).ready(function() {
 
   function leapYear(year) {
 
-    if((year % 100) === 0) {
-
-      return false;
-    } else if((year % 4) === 0) {
+    if((year % 4 === 0) && (year % 100 !== 0)) {
 
       return true;
     } else {
